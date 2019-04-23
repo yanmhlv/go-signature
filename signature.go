bf2674d81aeeb4a701ac1a2bf87e6886e3866b07
Yan Mikhailov <yanmhlv@gmail.com>
2019-04-23 10:48:43 +0300

    init

---
+++ signature.go
@@ -0,0 +1,59 @@
+package signature
+
+import (
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"errors"
+)
+
+func Sign(priv *rsa.PrivateKey, data []byte) ([]byte, error) {
+	h := sha256.New()
+	h.Write(data)
+	d := h.Sum(nil)
+	return rsa.SignPKCS1v15(rand.Reader, priv, crypto.SHA256, d)
+}
+
+func Unsign(pub *rsa.PublicKey, data, sign []byte) error {
+	h := sha256.New()
+	h.Write(data)
+	d := h.Sum(nil)
+	return rsa.VerifyPKCS1v15(pub, crypto.SHA256, d, sign)
+}
+
+func ParsePrivateKey(data []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	priv, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return priv, nil
+}
+
+func ParsePublicKey(data []byte) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode(data)
+	if block == nil {
+		return nil, errors.New("failed to parse PEM block containing the key")
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	switch pub := pub.(type) {
+	case *rsa.PublicKey:
+		return pub, nil
+	default:
+		break // fall through
+	}
+	return nil, errors.New("Key type is not RSA")
+}
